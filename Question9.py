91f61833ff74e0e7ff01034b15fb86e93b63690a
xuff10498 <zouchengxufei@163.com>
2017-12-12 17:42:05 +0800

    解决6个问题

---
+++ Question9.py
@@ -0,0 +1,31 @@
+'''
+a + b + c = 1000.
+a2+b2=C2
+求 abc的积
+'''
+const=1000
+def isExist():
+    a=1
+    b=1
+    c=1000-a-b
+    while(not pTrigle(a,b,c)):
+        b+=1
+        a=abc(b)
+        c = const - a - b
+        if(c<0):
+            break
+
+    print("a:%s b:%s c:%s"%(a,b,c))
+    print("result: %s"%(a*b*c))
+def abc(b):
+    if (2 * const - 2 * b)==0 :
+        return b
+    a = int((const ** 2 - 2 * const * b) / (2 * const - 2 * b))
+    return a
+
+def pTrigle(a,b,c):
+    if a**2+b**2==c**2:
+        return True
+    return False
+isExist()
+#a:375 b:200 c:425
